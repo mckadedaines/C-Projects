4c744cb20396cb9384ffc7e00e871e0fbdaedbb9
Mckade Daines <mckadedaines1@gmail.com>
2022-04-20 14:56:23 -0600

    Solo 1 Done

---
+++ solo1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.Write("What is your first name? ");
+string first = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string last = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last}, {first} {last}.");
