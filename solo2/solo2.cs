fda3e5abebf185f40577d191ef216f374c19d31f
Mckade Daines <mckadedaines1@gmail.com>
2022-04-25 15:00:18 -0600

    Ummm new update?

---
+++ solo2/solo2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Console.Write("Please enter your grade percent: ");
+string answer = Console.ReadLine();
+int percent = int.Parse(answer);
+
+string letter = "";
+
+if (percent >= 90) {
+    letter = "A";
+}
+
+else if (percent >= 80) {
+    letter = "B";
+}
+
+else if (percent >= 70) {
+    letter = "C";
+}
+
+else if (percent >= 60) {
+    letter = "D";
+}
+
+else {
+    letter = "F";
+}
+
+Console.WriteLine(letter);
---
+++ solo3/Program.cs
@@ -0,0 +1,18 @@
+﻿Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 11);
+
+Console.Write("What is  the magic number: ");
+string ans = Console.ReadLine();
+int num = int.Parse(ans);
+
+if (num == number) {
+    Console.WriteLine("You guessed it!");
+}
+
+else if (num > number) {
+    Console.WriteLine("Lower");
+}
+
+else {
+    Console.WriteLine("Higher");
+}
