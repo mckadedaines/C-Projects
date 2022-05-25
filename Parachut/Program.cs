ebab91ce2f969fabea428d93c9d770a0079dcc3c
Mckade Daines <mckadedaines1@gmail.com>
2022-05-25 14:24:18 -0600

    hangman

---
+++ Parachut/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Random;
+using System.Text;
+
+
+namespace Parachut {
+
+    public class Program {
+
+        static void Main(){
+
+            
+
+            Director director = new Director();
+            RandomWord random = new RandomWord();
+        }
+    }
+}
